b8081aa819477cd1f666bef015b05e344d0277d7
Tibeechaw <thibautscholaert@gmail.com>
2025-04-25 00:17:52 +0200

    .

--- components/sign-in-button.tsx
+++ components/sign-in-button.tsx
@@ -26,7 +26,7 @@ export default function SignInButton() {
             provider: 'google',
             options: {
                 redirectTo: `${window.location.origin}/auth/callback`,
-                scopes: 'https://www.googleapis.com/auth/youtube.readonly email profile'
+                // scopes: 'https://www.googleapis.com/auth/youtube.readonly email profile'
             }
         });
     };
